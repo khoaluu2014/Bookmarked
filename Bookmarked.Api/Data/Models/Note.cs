466e06e2fcaf98f8da13c8d1b64125df4fc300bb
khoaluu2014 <khoaulu2014@gmail.com>
2025-07-15 23:18:25 -0700

    delete old files

--- Bookmarked.Api/Data/Models/Note.cs
+++ Bookmarked.Api/Data/Models/Note.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BookMarked.Api.Data.Models;
 
 public class Note
 {
+    [Key]
     public int Id { get; set; }
     public required string Quote { get; set; }
     public string? Thoughts { get; set; }
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    [ForeignKey]
+    public int UserId { get; set; }
 }
