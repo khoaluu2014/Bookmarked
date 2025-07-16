ae015f7b4c22f7b088ffeb09bcba5c603e76966f
khoaluu2014 <khoaulu2014@gmail.com>
2025-07-15 23:02:04 -0700

    add Books model

--- Bookmarked.Api/Data/Models/Books.cs
+++ Bookmarked.Api/Data/Models/Books.cs
@@ -1,3 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookMarked.Api.Data.Models;
 
-public class Books { }
+public class Books
+{
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Text
+}
