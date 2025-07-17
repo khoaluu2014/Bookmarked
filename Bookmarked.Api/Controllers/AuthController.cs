09fc418ad96fa40bc958594c4b1485a7b92bbd46
khoaluu2014 <khoaluu2014@gmail.com>
2025-07-17 16:29:59 -0700

    initiate controller

---
+++ Bookmarked.Api/Controllers/AuthController.cs
@@ -0,0 +1,8 @@
+using BookMarked.Api.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMarked.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase { }
