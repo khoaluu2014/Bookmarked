88fd5617adfdf5af9c4fbcf885981f20886f538d
khoaluu2014 <khoaulu2014@gmail.com>
2025-07-17 22:02:26 -0700

    make register return ok

--- Bookmarked.Api/Controllers/AuthController.cs
+++ Bookmarked.Api/Controllers/AuthController.cs
@@ -5,4 +5,11 @@ namespace BookMarked.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AuthController : ControllerBase { }
+public class AuthController : ControllerBase
+{
+    [HttpPost("register")]
+    public IActionResult Register([FromBody] RegisterRequestDto request)
+    {
+        return Ok();
+    }
+}
